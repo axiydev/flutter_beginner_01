be07f7e5b6335782ba1c677b109cc79c3bba946f
Axmadjon Isaqov <67532499+axiydev@users.noreply.github.com>
2022-08-17 11:25:48 +0500

    animations

--- lib/pages/A/a_page.dart
+++ lib/pages/A/a_page.dart
@@ -13,13 +13,27 @@ class _APageState extends State<APage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('A page'),
+        centerTitle: true,
+        leading:
+            IconButton(onPressed: () {}, icon: const Icon(Icons.access_alarm)),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.home)),
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.notification_add)),
+        ],
+      ),
+      // drawer: const Drawer(),
+      // endDrawer: const Drawer(),
       body: Center(
         child: Text(
           'A Page',
           style: Theme.of(context).textTheme.headline1,
         ),
       ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context, CustomAnimationRoute(child: const BPage()));
